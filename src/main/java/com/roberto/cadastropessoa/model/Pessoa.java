7cb10491273390fe33c8bd03b327d028e9d3dfa4
robertocajueiro <beto.cajueiro@gmail.com>
2023-02-02 22:51:56 -0300

    Adicionar campo data de cadastro de pessoa

--- src/main/java/com/roberto/cadastropessoa/model/Pessoa.java
+++ src/main/java/com/roberto/cadastropessoa/model/Pessoa.java
@@ -1,10 +1,16 @@
 package com.roberto.cadastropessoa.model;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -30,7 +36,16 @@ public class Pessoa {
 	@NotNull
 	private Boolean status;
 	
+	@Column(name = "data_cadastro", updatable = false)
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataCadastro;
+	
 	@Embedded
 	private Endereco endereco;
+	
+	@PrePersist
+	public void prePersist() {
+		setDataCadastro(LocalDate.now());
+	}
 
 }
